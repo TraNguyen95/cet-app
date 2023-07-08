bca027be0b4afea9e3302e21817373ea64543421
tranguyen95 <nguyenhuongtra24@gmail.com>
2023-07-08 11:44:54 +0700

    setup codebase vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-center text-3xl font-bold text-red-500 underline'>Hello world!</h1>
+      <ToastContainer />
+    </div>
+  )
+}
+
+export default App
